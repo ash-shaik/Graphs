197a395dbc8500b2bc1ef0a4fd950714e48ed366
ash-shaik <fastmlai777@gmail.com>
2021-11-23 10:31:21 -0800

    Graph class

---
+++ Graph.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+
+
+class Graph:
+    """Undirected Graph class"""
+
+    def __init__(self, numNodes, edges):
+        self.adjList = defaultdict(list)
+        self.numNodes = numNodes
+        for v1, v2 in edges:
+            self.adjList[v1].append(v2)
+            self.adjList[v2].append(v1)
+
+    def dfs(self, source, visited):
+        """
+        Traverse graph from the source in preorder.
+        PreOrder : Visit each node before its children.
+        Time complexity : O(E + V), with num edges varying from O(1) in a Sparse Graph
+        to O(V2) in a Dense Graph.
+        :param source:
+        :param visited:
+        :return:
+
+        """
+        visited.append(source)
+        print(source, end=' ')
+        for neighbor in self.adjList[source]:
+            if neighbor not in visited:
+                self.dfs(neighbor, visited)
+
+
+def search_helper(graph):
+    visited = []
+
+    for i in range(graph.numNodes):
+        if i not in visited:
+            graph.dfs(i, visited)
+
+
+if __name__ == '__main__':
+    edges = [
+        # Notice that node 0 is unconnected
+        (1, 2), (1, 7), (1, 8), (2, 3), (2, 6), (3, 4),
+        (3, 5), (8, 9), (8, 12), (9, 10), (9, 11)
+    ]
+    n = 13
+
+    graph = Graph(13, edges)
+    search_helper(graph)
