46699c88ca5b0abd9567235c24efa3ba7dbf6e62
ash-shaik <fastmlai777@gmail.com>
2021-11-23 19:05:54 -0800

    Directed Graph: problems

--- DGraph.py
+++ DGraph.py
@@ -11,6 +11,12 @@ class DGraph:
             self.adjList[v1].append(v2)
 
     def canFinish(self, course, visited):
+        """
+        A DFS of course recursing through the edges.
+        :param course:
+        :param visited:
+        :return:
+        """
         visited[course] = 1
 
         for preRequisite in self.adjList[course]:
@@ -25,6 +31,7 @@ class DGraph:
 def canFinishCourseSchedule(courseSchedule):
     """
     Checks if a Course Schedule can be completed.
+    Method Uses topological sort to determine this.
     :param courseSchedule:
     :return:
     """
@@ -38,8 +45,48 @@ def canFinishCourseSchedule(courseSchedule):
     return True
 
 
+def alienDictionary(words):
+    """
+    #
+    :param words: Already sorted words from alien language
+    :return: Return a string of the unique letters in the new alien language
+             sorted in lexicographically increasing order by the new language's rules.
+    """
+    adjList = {letter: set() for word in words for letter in word}
+    for i in range(len(words) - 1):
+        word1, word2 = words[i], words[i + 1]
+        minLen = min(len(word1), len(word2))
+
+        if len(word1) > len(word2) and word1[:minLen] == word2[:minLen]:
+            return ""
+        for j in range(minLen):
+            if word1[j] != word2[j]:
+                adjList[word1[j]].add(word2[j])
+                break
+    visited = {}
+    result = []
+
+    def dfs(letter):
+        if letter in visited:
+            return visited[letter]
+        visited[letter] = True
+        for neighbor in adjList[letter]:
+            if dfs(neighbor):
+                return True
+        visited[letter] = False
+        result.append(letter)
+
+    for letter in adjList:
+        if dfs(letter):
+            return ""
+    result.reverse()
+    return "".join(result)
+
+
 if __name__ == '__main__':
     numCourses = 4
     preRequisites = [[1, 0], [1, 2], [3, 1], [0, 3]]
     courseSchedule = DGraph(numCourses, preRequisites)
     print(canFinishCourseSchedule(courseSchedule))
+
+    print(alienDictionary(["wrt", "wrf", "er", "ett", "rftt"]))
