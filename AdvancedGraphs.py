f43c1de7bd752e870961cf33bf17730cb113cd0c
ash-shaik <fastmlai777@gmail.com>
2021-11-26 18:54:35 -0800

    shortest path alternate

--- AdvancedGraphs.py
+++ AdvancedGraphs.py
@@ -46,9 +46,99 @@ def updateDistanceBFS(adjList, source, target, numVertex, parent, distance):
     return False
 
 
+def find_shortest_path_in_maze(grid):
+    start, goal, water, land = '@', '+', '#', '.'
+    keys, doors = {}, {}
+    lands = []
+
+    parent, distance = {}, {}
+
+    start_cord, goal_cord = None, None
+    rows, cols = len(grid), len(grid[0])
+
+    for r in range(rows):
+        for c in range(cols):
+            parent[(r, c)] = -1
+            distance[(r, c)] = float('inf')
+
+            coordinates = (r, c)
+            element = grid[r][c]
+
+            if element == start:
+                start_cord = coordinates
+                distance[start_cord] = 0
+            elif element == goal:
+                goal_cord = coordinates
+                distance[goal_cord] = 0
+            elif element in 'abcdefghijklmnopqrstuvwxyz':
+                keys[element] = coordinates
+            elif element in 'ABCDEFGHIJKLMNOPQRSTUVQXYZ':
+                doors[element] = coordinates
+            elif grid[r][c] == '.':
+                lands.append(coordinates)
+            else:
+                continue
+
+    def start_to_goal_bfs(start_cord, goal_cord, ):
+        # Tracks the keys visited if any.
+        visited = [start_cord]
+        queue = [start_cord]
+
+        while queue:
+            current = queue[0]
+            queue.pop(0)
+            if current == goal_cord:
+                break
+            r, c = current
+            directions = [[r - 1, c], [r, c + 1], [r + 1, c], [r, c - 1]]
+            for dr, dc in directions:
+                r, c = dr, dc
+                neighbor = (r, c)
+                if 0 <= r < rows and 0 <= c < cols and grid[r][c] != start and grid[r][c] != water \
+                        and (r, c) not in visited:
+                    currentElement = grid[r][c]
+                    # if currentElement == goal:
+                    #    break
+                    # if the element is a door, check if its key has been visited.
+                    if currentElement in doors and currentElement.lower() in keys and keys[
+                        currentElement.lower()] not in visited:
+                        continue
+                    # add to visited if we see a key.
+                    # if currentElement in keys:
+                    visited.append(neighbor)
+                    distance[neighbor] = distance[current] + 1
+                    parent[neighbor] = current
+                    queue.append(neighbor)
+
+    start_to_goal_bfs(start_cord, goal_cord)
+    path = []
+    current = goal_cord
+    x, y = current
+    path.append([x, y])
+
+    while parent[current] != -1:
+        # x, y = parent[current]
+        # path.append([x, y])
+        path.append(parent[current])
+        current = parent[current]
+    path.reverse()
+    return [list(cord) for cord in path]
+
+
 if __name__ == '__main__':
     numVertices = 8
     edges = [[0, 1], [0, 3], [1, 2], [3, 4], [3, 7], [4, 5], [4, 6]
         , [4, 7], [5, 6], [6, 7]]
 
     shortestDistance(numVertices, 0, 7, edges)
+    grid = ["...B"
+        , ".b#."
+        , "@#+."]
+
+    # grid = ["+B..."
+    #     , "####."
+    #     , "##b#."
+    #     , "a...A"
+    #     , "##@##"]
+
+    print(find_shortest_path_in_maze(grid))
---
+++ ShortestPath.py
@@ -0,0 +1,102 @@
+'''
+ The key in finding a shortest path from source to goal is to :
+ * Build up the distance of each vertex from the source
+ * the parent vertex in the path from the source.
+ * then compute the shortest path from source to destination.
+
+ # In the solution , we not just track the visited cells but also the keys in our possession
+ as we traverse the maze.
+ The possession of the keys we collection along the traversal - we will call it the keyring here.
+ That way we allow visiting a cell , when approached with a new set of keyring.
+
+ we'll use a 10-bit integer to represent the keyring.
+ water	don’t include
+ locked door	don’t include
+ door w/key	include
+ land	include
+ door or land w/keyring already seen	don’t include
+
+'''
+
+import heapq
+
+from collections import defaultdict, deque
+
+WATER = '#'
+
+
+def find_shortest_path_in_grid(grid):
+    rows, cols = len(grid), len(grid[0])
+    start, end = None, None
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == '@':
+                start = (i, j)
+            if grid[i][j] == '+':
+                end = (i, j)
+
+    keyRing = 0
+    visited = defaultdict(list)
+    visited[(start[0], start[1])].append(keyRing)
+    parents = {(start[0], start[1], keyRing): None}
+
+    q = deque()
+    q.append((start[0], start[1], keyRing))
+    while q:
+        current, key = q.popleft()
+
+        if (current[0], current[1]) == end:
+            break
+        for neighbor in getNeighbors(grid, current[0], current[1]):
+            nRow, nCol, newKeyRing = neighbor[0], neighbor[1], neighbor[2]
+            if not isVisited(nRow, nCol, newKeyRing, visited):
+                q.append((nRow, nCol, newKeyRing))
+                parents[(nRow, nCol, newKeyRing)] = current[0], current[1], keyRing
+                visited[(nRow, nCol)].append(newKeyRing)
+
+
+def getNeighbors(grid, row, col, keyRing):
+    neighbors = []
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    rows, cols = len(grid), len(grid[0])
+
+    for dr, dc in directions:
+        new_row, new_col = row + dr, col + dc
+        if 0 <= new_row < rows and 0 <= new_col < cols:
+            value = grid[new_row][new_col]
+            if value == WATER:
+                continue
+            # check for access to door.
+            if value in 'ABCDEFGHIJ':
+                if keyRing & (1 << (ord(value) - ord('A'))) == 0:
+                    continue
+            if value in 'abcdefghij':
+                # we found a key so we'll add it in our keyring
+                newKeyRing = keyRing | (1 << (ord(value) - ord('a')))
+            else:
+                newKeyRing = keyRing
+            neighbors.append((new_row, new_col, newKeyRing))
+    return neighbors
+
+
+def isVisited(new_row, new_col, newKeyring, visited):
+    for key in visited[(new_row, new_col)]:
+        if newKeyring == key:
+            return True
+    return False
+
+
+'''
+ We'll use bitwise operations and bit masks to find if we have a key to a door.
+ mask - bits you want to keep and which bits you want to clear.
+ 
+'''
+if __name__ == '__main__':
+    grid = ["+B..."
+        , "####."
+        , "##b#."
+        , "a...A"
+        , "##@##"]
+
+    print(find_shortest_path_in_grid(grid))
