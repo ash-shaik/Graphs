1491ce909fe28c906bfaaf99f9e91ecabc5ba841
ash-shaik <fastmlai777@gmail.com>
2021-12-07 10:51:01 -0800

    cycle paths of length n

--- GraphPaths.py
+++ GraphPaths.py
@@ -7,6 +7,10 @@ class AMGraph:
 
     def __init__(self, nV, edges):
         self.numVertices = nV
+        """
+         Bit mask optimization, 
+         store A[i][j] as bits of 32 or 64-bit integers.
+        """
         self.adjMat = [[0 for i in range(nV)] for j in range(nV)]
         for u, v in edges:
             self.adjMat[u][v] = 1
@@ -32,7 +36,9 @@ class AMGraph:
         Check if there exits a cycle of length n.
         :param n: length of cycle we are looking for.
         :return: boolean, True if cycle exists, False if not.
-        Time Complexity : O(|V|3)
+        Time Complexity : O(|V|3), lets improve this by bitmask optimization
+        Allows for parallel operations.
+
         """
         N = self.numVertices
         for u in range(N):
@@ -43,6 +49,26 @@ class AMGraph:
                 for w in range(v + 1, N):
                     if self.adjMat[u][w] == 1 and self.adjMat[v][w] == 1:
                         return True
+        return False
+
+    def cycleOfLength4(self):
+        """
+        Check if there exits a cycle of length 4.
+        :return: boolean, True if cycle exists, False if not.
+        Time Complexity : O(|V|4)
+        """
+        N = self.numVertices
+        for u in range(N):
+            for v in range(u + 1, N):
+                if self.adjMat[u][v] == 0:
+                    continue
+                # if there is an edge between u and v, continue to third point
+                for w in range(v + 1, N):
+                    if self.adjMat[v][w] == 0:
+                        continue
+                    for x in range(w + 1, N):
+                        if self.adjMat[w][x] == 1 and self.adjMat[u][x] == 1:
+                            return True
         return False
 
 
@@ -56,4 +82,4 @@ if __name__ == '__main__':
         [0, 1], [0, 3], [1, 2], [1, 4], [2, 3], [3, 4]
     ]
     aGraph = AMGraph(5, edges)
-    print(aGraph.cycleOfLength(3))
+    print(aGraph.cycleOfLength4())
