09cafd075ee0105d94b073aa509132d59b30c1af
ash-shaik <fastml777@gmail.com>
2022-01-21 16:18:27 -0800

    bidirectional search

--- BidirectionalSearch.py
+++ BidirectionalSearch.py
@@ -12,8 +12,8 @@ class BidirectionalSearch:
         self.forward_queue = []
         self.backward_queue = []
 
-        self.visited_forward = []
-        self.visited_backward = []
+        self.visited_forward = [False] * self.num_vertices
+        self.visited_backward = [False] * self.num_vertices
 
         self.parent_forward = [None] * self.num_vertices
         self.parent_backward = [None] * self.num_vertices
@@ -22,28 +22,39 @@ class BidirectionalSearch:
         path = []
 
         self.forward_queue.append(start)
-        self.visited_forward.append(start)
+        self.visited_forward[start] = True
         self.parent_forward[start] = -1
 
         self.backward_queue.append(goal)
-        self.visited_backward.append(goal)
+        self.visited_backward[goal] = True
         self.parent_backward[goal] = -1
+        return path
 
     def bfs(self, direction=1):
-
         if direction:
             # forward
             while self.forward_queue:
                 current = self.forward_queue.pop(0)
                 for neighbor in self.adjList[current]:
-                    if neighbor not in self.visited_forward:
+                    if not self.visited_forward[neighbor]:
+                        self.visited_forward[neighbor] = True
                         self.forward_queue.append(neighbor)
+                        self.parent_forward[neighbor] = current
 
         else:
-            current = self.backward_queue.pop(0)
-            connect_node = self.adjList[current]
+            while self.backward_queue:
+                current = self.backward_queue.pop(0)
+                for neighbor in self.adjList[current]:
+                    if not self.visited_backward[neighbor]:
+                        self.visited_backward[neighbor] = True
+                        self.backward_queue.append(neighbor)
+                        self.parent_backward[neighbor] = current
 
-        return path
+    def has_common(self):
+        for i in range(self.num_vertices):
+            if self.visited_forward[i] and self.visited_backward[i]:
+                return i
+        return -1
 
 
 if __name__ == '__main__':
