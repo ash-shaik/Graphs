6f9a07eb73b8e920e9f61934c7729cd9ce093e22
ash-shaik <fastml777@gmail.com>
2022-01-21 17:25:48 -0800

    bidirectional search

--- BidirectionalSearch.py
+++ BidirectionalSearch.py
@@ -18,8 +18,23 @@ class BidirectionalSearch:
         self.parent_forward = [None] * self.num_vertices
         self.parent_backward = [None] * self.num_vertices
 
+    def get_path(self, common_node, start, goal):
+        path = [start]
+        current = common_node
+
+        while current != start:
+            path.append(self.parent_forward[current])
+            current = self.parent_forward[current]
+        path = path[::-1]
+
+        current = common_node
+        while current != goal:
+            path.append(self.parent_backward[current])
+            current = self.parent_backward[current]
+
+        print(''.join(str(path)))
+
     def bidirectional_search(self, start, goal):
-        path = []
 
         self.forward_queue.append(start)
         self.visited_forward[start] = True
@@ -28,7 +43,16 @@ class BidirectionalSearch:
         self.backward_queue.append(goal)
         self.visited_backward[goal] = True
         self.parent_backward[goal] = -1
-        return path
+
+        while self.forward_queue and self.backward_queue:
+            self.bfs(1)
+            self.bfs(0)
+            common_node = self.has_common()
+            if common_node != -1:
+                print("Path found")
+                path = self.get_path(common_node, start, goal)
+                return path
+        return -1
 
     def bfs(self, direction=1):
         if direction:
@@ -59,7 +83,10 @@ class BidirectionalSearch:
 
 if __name__ == '__main__':
     num_vertices = 17
+    start, goal = 1, 16
     edges = [(1, 4), (2, 4), (3, 6), (5, 6), (4, 8), (6, 8), (8, 9), (9, 10)
         , (10, 11), (11, 13), (11, 14), (10, 12), (12, 15), (12, 16)]
 
-    graph = BidirectionalSearch()
+    graph = BidirectionalSearch(num_vertices, edges)
+    found_path = graph.bidirectional_search(start, goal)
+    print(found_path)
